9173c1e3993ae84227edc11857dfee538d34463e
DavidJames-Tech <davidadakole21@gmail.com>
2023-10-09 14:46:46 +0100

    Setup mui for my frontend

--- frontend/@types/type.d.ts
+++
@@ -1,4 +0,0 @@
-
-declare type SessionData = {
-    label: string
-}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/teleserve.png'
+import twaLogo from './assets/tapps.png'
+import viteLogo from '/vite.svg'
 import './App.css'
+
+import WebApp from '@twa-dev/sdk'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,25 +12,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          Welcome to Teleserve
       </div>
-      <h1 className="bg-red-500 px-4 py-2 rounded-2xl">Teleserve + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
